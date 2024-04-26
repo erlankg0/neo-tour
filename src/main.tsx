dfa6f1cc0f0bd7fa8a29438270083a8a5c600134
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-04-26 23:11:52 +0300

    HashRouter, vite config for fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './layout/App/App.tsx'
 import './index.css'
 import './normalize.css'
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
     ,
 )
